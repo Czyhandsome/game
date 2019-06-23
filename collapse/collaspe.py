3876ab36f03df42e95fe17ab158635ca39054c87
caoziyu <czyhandsome@foxmail.com>
2019-06-23 09:59:36 +0800

    使用clock.tick()控制帧数

--- collapse/collaspe.py
+++ collapse/collaspe.py
@@ -5,14 +5,21 @@ from collapse.Sony import Sony
 
 
 if __name__ == '__main__':
+    # Init main frame
     width, height = 640, 480
     pygame.init()
     screen = pygame.display.set_mode([width, height])
     screen.fill([255, 255, 255])
+
+    # Get a clock
+    clock = pygame.time.Clock()
+
+    # Print red face
     red_face = RedFace([20, 20], width, height)
     screen.blit(red_face.image, red_face.rect)
     pygame.display.flip()
 
+    # Print sony
     sony = Sony([220, 20], width, height)
     screen.blit(sony.image, sony.rect)
     pygame.display.flip()
@@ -23,7 +30,8 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        pygame.time.delay(5)
+        clock.tick(120)
+        print(clock.get_fps())
         screen.fill([255, 255, 255])
         # Move red_face
         red_face.move()
